6247cb726f76750b745c2909eb2b1ea06b512bb7
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2016-03-16 18:47:31 -0300

    license

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description='Miscellaneous methods for astronomy, dealing with arrays, statistical distributions and computing goodness-of-fit',
     long_description=readme,
     author='Rodrigo Nemmen',
-    author_email='me@kennethreitz.com',
+    author_email='rodrigo.nemmen@iag.usp.br',
     url='https://github.com/kennethreitz/samplemod',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
