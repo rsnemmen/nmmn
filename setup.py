4f3ad7964e95b9a5f625e6ef415e1fa69edf330a
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2016-03-16 18:43:02 -0300

    setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='nemmen',
+    version='0.1',
+    description='Miscellaneous methods for astronomy, dealing with arrays, statistical distributions and computing goodness-of-fit',
+    long_description=readme,
+    author='Rodrigo Nemmen',
+    author_email='me@kennethreitz.com',
+    url='https://github.com/kennethreitz/samplemod',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
