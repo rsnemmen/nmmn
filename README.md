6458b6a849b693fb680fe6ed7e8dab4f9dc9df0c
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2016-08-26 07:01:22 -0300

    working version of grmhd, but reading grid incorrectly

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Miscellaneous modules for:
 * `plots`: custom plots
 * `fermi`: Fermi LAT analysis methods
 * `bayes`: Bayesian tools for dealing with posterior distributions
+* `grmhd`: tools for dealing with GRMHD numerical simulations
 
 These are modules I wrote which I find useful -- for whatever reason -- in my research.
 
--- nmmn/grmhd.py
+++ nmmn/grmhd.py
@@ -35,10 +35,8 @@ Reads data from a VTK file:
 >>> o.vtk("ok200.vtk")
 	"""
 
-	def __init__(self):
-		# does nothing for now
-
-
+	#def __init__(self):
+	# does nothing for now
 
 
 
@@ -47,6 +45,8 @@ Reads data from a VTK file:
 	Given a VTK file created with the RAISHIN GRMHD code, this reads the
 	data as numpy arrays.
 		"""
+		import re
+
 		f = open(vtkfile,"r")
 		newf=open("tmp.dat","w")	# file that will hold coordinates
 
@@ -57,71 +57,84 @@ Reads data from a VTK file:
 		strd,strp,strvx,strvy,strvz,strb2,strbx,strby,strbz='','','','','','','','','' # string that holds values 
 
 		for line in f:
-		    # gets dimensions
-		    if re.search(r'DIMENSIONS\s+\d+\s+\d+',line):
-		        s=re.findall(r'\s*\d+\s*',line.rstrip('\n'))
-		        nx=int(s[0])
-		        ny=int(s[1])
-		        nz=int(s[2])
+			# gets dimensions
+			if re.search(r'DIMENSIONS\s+\d+\s+\d+',line):
+				s=re.findall(r'\s*\d+\s*',line.rstrip('\n'))
+				nx=int(s[0])
+				ny=int(s[1])
+				nz=int(s[2])
 		        
 		    # gets mesh: number number 
-		    if re.search(r'-?\d+\.\d+E?[-+]?\d+\s+-?\d+\.\d+E?[-+]?\d+\s+-?\d+\.\d+E?[-+]?\d+',line):
-		        newf.write(line)
+			if re.search(r'-?\d+\.\d+E?[-+]?\d+\s+-?\d+\.\d+E?[-+]?\d+\s+-?\d+\.\d+E?[-+]?\d+',line):
+				newf.write(line)
 		            
-		    # gets arrays
-		    # these lines are important to tell python when to stop reading shit
-		    # it must be sequential
-		    if 'density' in line: boold=True
-		    if 'pressure' in line: 
-		    	boold=False
-		    	boolp=True
-		    if 'LorentzW1' in line:
-		    	boolp=False
-		    if 'util^x' in line:
-		        boolvx=True
-		    if 'util^y' in line:
-		        boolvx=False
-		        boolvy=True    
-		    if 'util^z' in line:
-		        boolvy=False
-		        boolvz=True  
-		    if 'b^2' in line:
-		        boolvz=False
-		        boolb2=True    		          
-		    if 'bx' in line:
-		    	boolb2=False
-		        boolbx=True
-		    if 'by' in line:
-		        boolbx=False
-		        boolby=True    
-		    if 'bz' in line: 
-		    	boolby=False    
-		    	boolbz=True
+			# gets arrays
+			# these lines are important to tell python when to stop reading shit
+			# it must be sequential
+			if 'density' in line: boold=True
+			if 'pressure' in line: 
+				boold=False
+				boolp=True
+			if 'LorentzW1' in line:
+				boolp=False
+			if 'util^x' in line:
+				boolvx=True
+			if 'util^y' in line:
+				boolvx=False
+				boolvy=True    
+			if 'util^z' in line:
+				boolvy=False
+				boolvz=True  
+			if 'b^2' in line:
+				boolvz=False
+				boolb2=True    		          
+			if 'bx' in line:
+				boolb2=False
+				boolbx=True
+			if 'by' in line:
+				boolbx=False
+				boolby=True    
+			if 'bz' in line: 
+				boolby=False    
+				boolbz=True
 		    
-		    if boold==True and re.search(r'-?\d+\.\d+E?[-+]?\d+',line):
-		        strd=strd+line
-		    if boolp==True and re.search(r'-?\d+\.\d+E?[-+]?\d+',line):
-		        strp=strp+line
-		    if boolvx==True and re.search(r'-?\d+\.\d+E?[-+]?\d+',line):
-		        strvx=strvx+line
-		    if boolvy==True and re.search(r'-?\d+\.\d+E?[-+]?\d+',line):
-		        strvy=strvy+line
-		    if boolvz==True and re.search(r'-?\d+\.\d+E?[-+]?\d+',line):
-		        strvz=strvz+line
-		    if boolb2==True and re.search(r'-?\d+\.\d+E?[-+]?\d+',line):
-		        strb2=strb2+line
-		    if boolbx==True and re.search(r'-?\d+\.\d+E?[-+]?\d+',line):
-		        strbx=strbx+line		        
-		    if boolby==True and re.search(r'-?\d+\.\d+E?[-+]?\d+',line):
-		        strby=strby+line
-		    if boolbz==True and re.search(r'-?\d+\.\d+E?[-+]?\d+',line):
-		        strbz=strbz+line
+			if boold==True and re.search(r'-?\d+\.\d+E?[-+]?\d+',line):
+				strd=strd+line
+			if boolp==True and re.search(r'-?\d+\.\d+E?[-+]?\d+',line):
+				strp=strp+line
+			if boolvx==True and re.search(r'-?\d+\.\d+E?[-+]?\d+',line):
+				strvx=strvx+line
+			if boolvy==True and re.search(r'-?\d+\.\d+E?[-+]?\d+',line):
+				strvy=strvy+line
+			if boolvz==True and re.search(r'-?\d+\.\d+E?[-+]?\d+',line):
+				strvz=strvz+line
+			if boolb2==True and re.search(r'-?\d+\.\d+E?[-+]?\d+',line):
+				strb2=strb2+line
+			if boolbx==True and re.search(r'-?\d+\.\d+E?[-+]?\d+',line):
+				strbx=strbx+line		        
+			if boolby==True and re.search(r'-?\d+\.\d+E?[-+]?\d+',line):
+				strby=strby+line
+			if boolbz==True and re.search(r'-?\d+\.\d+E?[-+]?\d+',line):
+				strbz=strbz+line
 
 		# gets numpy arrays finally
+		self.rho=numpy.fromstring(strd, sep='\n')
+		self.p=numpy.fromstring(strp, sep='\n')
+		self.vx=numpy.fromstring(strvx, sep='\n')
+		self.vy=numpy.fromstring(strvy, sep='\n')
+		self.vz=numpy.fromstring(strvz, sep='\n')
+		self.b2=numpy.fromstring(strb2, sep='\n')
+		self.bx=numpy.fromstring(strbx, sep='\n')
+		self.by=numpy.fromstring(strby, sep='\n')
+		self.bz=numpy.fromstring(strbz, sep='\n')
 
-		        
+		# reads mesh positions 
+		self.x,self.y,self.z= numpy.loadtxt('tmp.dat',unpack=True,usecols=(0,1,2))
+
+		# close files        
 		f.close()
 		newf.close()
+
 
 
 
