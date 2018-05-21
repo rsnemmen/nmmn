c1b7c18d8ae390693c83bf2b5aa5f35eace962c4
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2018-05-21 21:49:57 +0200

    added MANIFEST to avoid issues with installation using pip

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ with open('LICENSE') as f:
 
 setup(
     name='nmmn',
-    version='0.8.1',
+    version='0.8.2',
     description='Miscellaneous methods for astronomy, dealing with arrays, statistical distributions and computing goodness-of-fit',
     long_description=readme,
     author='Rodrigo Nemmen',
     author_email='rodrigo.nemmen@iag.usp.br',
     url='https://github.com/rsnemmen/nmmn',
-    download_url = 'https://github.com/peterldowns/mypackage/archive/0.8.1.tar.gz', # I'll explain this in a second
+    download_url = 'https://github.com/peterldowns/mypackage/archive/0.8.2.tar.gz', # I'll explain this in a second
     license=license,
     keywords = ['science', 'statistics', 'signal-processing', 'numerical-methods', 'astronomy', 'numerical-simulations', 'astrophysics', 'mhd', 'grmhd'], # arbitrary keywords
     packages=find_packages(exclude=('tests', 'docs'))
