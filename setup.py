87ac14e68e1b3f87c3adffdf67face08331a588a
Rodrigo Nemmen <rodrigo.nemmen@iag.usp.br>
2019-06-03 12:21:58 -0300

    several errors trying to upload project to pypi
    • error "long_description_content_type": added the line long_description_content_type='text/markdown', to setup.py
    • error continued: fixed the license line

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
+#with open('LICENSE') as f:
+#    license = f.read()
 
 setup(
     name='nmmn',
@@ -16,8 +16,7 @@ setup(
     author_email='rodrigo.nemmen@iag.usp.br',
     url='https://github.com/rsnemmen/nmmn',
     download_url = 'https://github.com/rsnemmen/nmmn/archive/0.8.6.tar.gz', # I'll explain this in a second
-    license=license,
+    license="MIT License",
     keywords = ['science', 'statistics', 'signal-processing', 'numerical-methods', 'astronomy', 'numerical-simulations', 'astrophysics', 'mhd', 'grmhd'], # arbitrary keywords
     packages=find_packages(exclude=('tests', 'docs'))
-)
-
+    )
