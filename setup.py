07bb447874a55a325d1f87aa35407d186f4e9697
Rodrigo Nemmen da Silva <rodrigo.nemmen@iag.usp.br>
2023-03-02 12:13:42 -0800

    forgot to include new version in setup, as usual

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ with open('README.md') as f:
 
 setup(
     name='nmmn',
-    version='1.2',
+    version='1.2.1',
     description='Miscellaneous methods for astronomy, dealing with arrays, statistical distributions and computing goodness-of-fit',
     long_description=readme,
     long_description_content_type='text/markdown',
     author='Rodrigo Nemmen',
     author_email='rodrigo.nemmen@iag.usp.br',
     url='https://github.com/rsnemmen/nmmn',
-    download_url = 'https://github.com/rsnemmen/nmmn/archive/1.2.tar.gz', # I'll explain this in a second
+    download_url = 'https://github.com/rsnemmen/nmmn/archive/1.2.1.tar.gz', # I'll explain this in a second
     license="MIT License",
     keywords = ['science', 'statistics', 'signal-processing', 'numerical-methods', 'astronomy', 'numerical-simulations', 'astrophysics', 'mhd', 'grmhd'], # arbitrary keywords
     packages=find_packages(exclude=('tests', 'docs'))
