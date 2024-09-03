147ef2b32a6a9f4966c8792af54ce8885e5c5945
Rodrigo Nemmen <rsnemmen@gmail.com>
2024-09-03 11:47:17 -0700

    updated config for pypi

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ with open('README.md') as f:
 
 setup(
     name='nmmn',
-    version='1.2.1',
-    description='Miscellaneous methods for astronomy, dealing with arrays, statistical distributions and computing goodness-of-fit',
+    version='1.3.6',
+    description='Miscellaneous methods for data science and astronomy',
     long_description=readme,
     long_description_content_type='text/markdown',
     author='Rodrigo Nemmen',
     author_email='rodrigo.nemmen@iag.usp.br',
     url='https://github.com/rsnemmen/nmmn',
-    download_url = 'https://github.com/rsnemmen/nmmn/archive/1.2.1.tar.gz', # I'll explain this in a second
+    download_url = 'https://github.com/rsnemmen/nmmn/archive/1.3.6.tar.gz', 
     license="MIT License",
     keywords = ['science', 'statistics', 'signal-processing', 'numerical-methods', 'astronomy', 'numerical-simulations', 'astrophysics', 'mhd', 'grmhd'], # arbitrary keywords
     packages=find_packages(exclude=('tests', 'docs'))
